5fc5a5131861927b636a20e2b7d6425fdca04e53
XL Naruto <xpertlab.naruto@gmail.com>
2026-06-01 16:55:36 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,20 +5,20 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
-  const apiTarget =
-    env.VITE_APP_API_TARGET || "https://api.dev.rateandreact.com/";
+  // const apiTarget =
+  //   env.VITE_APP_API_TARGET || "https://api.dev.rateandreact.com/";
   return {
     base: env.VITE_APP_BASE_URL || "/",
     plugins: [react(), tailwindcss()],
-    server: {
-      proxy: {
-        "/api-proxy": {
-          target: apiTarget,
-          changeOrigin: true,
-          secure: false,
-          rewrite: (path) => path.replace(/^\/api-proxy\//, ""),
-        },
-      },
-    },
+    // server: {
+    //   proxy: {
+    //     "/api-proxy": {
+    //       target: apiTarget,
+    //       changeOrigin: true,
+    //       secure: false,
+    //       rewrite: (path) => path.replace(/^\/api-proxy\//, ""),
+    //     },
+    //   },
+    // },
   };
 });
