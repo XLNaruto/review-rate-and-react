272e613cd22dba576e78fa6e2615d8c7d6e33009
XL Naruto <xpertlab.naruto@gmail.com>
2026-06-01 16:59:37 +0530

    changes

--- src/utils/ApiHelper.ts
+++ src/utils/ApiHelper.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const URL = {
-  apiUrl: import.meta.env.VITE_APP_API_URL
+  apiUrl: import.meta.env.VITE_APP_API_TARGET
 };
 
 export const Securitykey = import.meta.env.VITE_APP_ENCRYPT_KEY || '';
